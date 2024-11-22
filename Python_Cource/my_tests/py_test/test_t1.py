ec011b7fa87bae3320d58f6eacfec65afa31fd65
ol4ik433 <ol4ikvorobey@gmail.com>
2024-11-22 14:25:07 +0300

    diplom

--- Python_Cource/my_tests/py_test/test_t1.py
+++ Python_Cource/my_tests/py_test/test_t1.py
@@ -1,26 +1,43 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import requests
 
 
-url = 'https://api.unsplash.com/photos/'
-query={"nature"}
-#q = {"city name":'London'}
-#appid={"d5b6760ee55b6f35b8a3ce79962e1575"}
-response = requests.get(url,params=query)
 
 
-print(response.url)
-print(response.json())
+import requests
 
 
+api_key = 'e430bdacdcc84f00893a16e0404019a7'
+ingredients = 'apple,flour,sugar'
+url = f'https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients}&apiKey={api_key}'
 
+response = requests.get(url)
+recipes = response.json()
 
+for recipe in recipes:
+       title = recipe['title']
+       image = recipe['image']
+       print(f'Название рецепта: {title}')
+       print(f'Ссылка на изображение: {image}')
 
-        #driver.find_element(By.ID,'verify').click()
+try:
+    #инициализирую драйвер
+    driver = webdriver.Chrome()
+    #говорим WebDriver каждый элемент искать 5 секунд.
+    # путь до страницы
+    driver.get('https://erikdark.github.io/recept_api/')
 
-
-       # WebDriverWait(driver,2).until(EC.visibility_of_element_located((By.ID,'verify_message')))
-
-
-
-
-       # assert driver.find_element(By.ID,'verify_message').text == 'Verification successful!','текст должен быть Verification successful!'
+a='Cranberry Apple Crisp'
+img='https://img.spoonacular.com/recipes/640352-312x231.jpg'
+#y= driver.find./element(results)
+#c= driver.find./element(results)
+#if a=y
+#b= c
+ if driver.find_element(By.ID,'results').text == 'Recipe: Cranberry Apple Crisp':
+        print('ok')
+    else:
+        print('ne okey')
